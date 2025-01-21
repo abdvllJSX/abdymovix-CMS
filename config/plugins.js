a5c8d6937978d4ad064209c4a0bce72d280bfb9b
Abdullahi Alimi <abdullahi.alimi@nematix.com>
2025-01-21 16:23:01 +0100

    fucking shit up

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,1 @@
-// config/plugins.js
-module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        providerOptions: {
-          sizeLimit: 250000 // example size in bytes
-        }
-      }
-    }
-  });
+module.exports = () => ({});
