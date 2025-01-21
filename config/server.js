d1a14b8b8b9768f1cb458cea77f0b4bfee9ed746
Abdullahi Alimi <abdullahi.alimi@nematix.com>
2025-01-21 12:52:33 +0100

    trynna fix deployment issue

--- config/server.js
+++ config/server.js
@@ -1,6 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: "https://abdycms.netlify.app",
+  admin: {
+    url: "https://abdycms.netlify.app",
+    serveAdminPanel: false,
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
