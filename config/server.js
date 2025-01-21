20f48c0b289f172d25459dd093b440dc4465e821
Abdullahi Alimi <abdullahi.alimi@nematix.com>
2025-01-21 13:15:50 +0100

    deploying

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: "https://abdycms.netlify.app",
+  url: env('BACKEND_URL', 'https://abdymovix-cms.onrender.com'), // Set to your backend's URL
   admin: {
-    url: "https://abdycms.netlify.app",
-    serveAdminPanel: false,
+    url: 'https://abdycms.netlify.app/admin', // URL of the Netlify-deployed admin panel
+    serveAdminPanel: false, // Ensure the backend does not try to serve the admin panel
   },
   app: {
     keys: env.array('APP_KEYS'),
