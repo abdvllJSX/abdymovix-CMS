122caae4191dacfbb003cd1ac6aea25eebc6aa84
Abdullahi Alimi <abdullahi.alimi@nematix.com>
2025-01-21 12:37:36 +0100

    first commit

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,35 @@
+// config/middlewares.js
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:'],
+          'media-src': ["'self'", 'data:', 'blob:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '256mb', // Increase form body size limit
+      jsonLimit: '256mb', // Increase JSON body size limit
+      textLimit: '256mb', // Increase text body size limit
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024, // Increase file size limit to 250mb
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
-  'strapi::body',
-  'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  about controller
+ * project controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::about.about');
+module.exports = createCoreController('api::project.project');
